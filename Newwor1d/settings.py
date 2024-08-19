803f2b217fee0949d4d24425f5b17500fa7ca427
Anudeep151998 <anudeepburra18@gmail.com>
2024-08-19 16:30:11 +0530

    commit changes

--- Newwor1d/settings.py
+++ Newwor1d/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3OKQZ36nOkkJy-lbfqXvRjRPjSUNMcDF5dm-ii6oOeV4biA_TyZpINKwMGTYEydzdDE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
