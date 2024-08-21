e51e38718f9cdae693acad17f16285fd47ee41c1
Anudeep151998 <anudeepburra18@gmail.com>
2024-08-21 19:28:31 +0530

    commit

--- Newwor1d/settings.py
+++ Newwor1d/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 
 # Add the path to the directory where your static files are located
 STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'itreporting/static'),
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
